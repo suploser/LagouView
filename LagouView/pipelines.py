2d79cb3b6953163668f1096f063162ddce35d1f8
ubuntu-user <sup_loser@outlook.com>
2018-01-29 01:37:33 +0900

    写入csv文件

--- LagouView/pipelines.py
+++ LagouView/pipelines.py
@@ -10,7 +10,6 @@ class LagouviewPipeline(object):
     def process_item(self, item, spider):
         data = [[item['job_name'], item['work_years'], item['degree'], item['job_type'], item['job_address'], item['salary'], item['welfare'], item['company']]]
         data = pd.DataFrame(data)
-        print(data)
         csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__),'..'))
         csv_path = os.path.join(csv_path, 'Lagou.csv')
         data.to_csv(csv_path, header=False, index=False, mode='a+')
